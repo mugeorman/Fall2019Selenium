8aca0b068b1eb512f34babb7799503cdaa381ee6
mugeorman <mugeorman@gmail.com>
2020-03-03 20:18:19 -0500

    Finding element updated

--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -2,11 +2,13 @@ package com.automation.tests.day3;
 
 import com.automation.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindElementsPractice {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
 
 //        WebDriverManager.chromedriver().setup();
@@ -14,6 +16,26 @@ public class FindElementsPractice {
 
         WebDriver driver= DriverFactory.createDriver("chrome");
         driver.get("http://practice.cybertekschool.com/sign_up");
+
+        WebElement fullName =driver.findElement(By.name("full_name"));
+        fullName.sendKeys("Mister Twister");
+
+        Thread.sleep(2000);
+
+        WebElement email= driver.findElement(By.name("email"));
+        email.sendKeys("sdet@cybertek.com");
+
+        Thread.sleep(2000);
+
+        WebElement signUp= driver.findElement(By.name("wooden_spoon"));
+       // when you see type="submit" , you can use submit() instead of click()
+       // it makes sense to use when click() method doesn't work
+        signUp.submit();
+
+        Thread.sleep(2000);
+
+
+
         driver.quit();
 
 
