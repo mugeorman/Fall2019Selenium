36d1672fd8fe28367adb37ba04d68577d6035a37
mugeorman <mugeorman@gmail.com>
2020-03-08 12:54:32 -0400

    select by text multiple option

---
+++ src/test/java/com/automation/tests/day6/SelectByTextMultipleOptions.java
@@ -0,0 +1,45 @@
+package com.automation.tests.day6;
+
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class SelectByTextMultipleOptions {
+    public static void main(String[] args) {
+
+        WebDriver driver= DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/dropdown");
+        BrowserUtils.wait(3);
+
+        Select languagesSelect= new Select(driver.findElement(By.name("Languages")));
+        // whether this select element support selecting multiple options at the same time?
+        //this is done by checking the value of the "multiple" attribute
+        boolean isMultiple=languagesSelect.isMultiple();
+        System.out.println("isMultiple = " + isMultiple);// if it is true, you can select more than one option
+
+        languagesSelect.selectByVisibleText("Java");
+        languagesSelect.selectByVisibleText("JavaScript");
+        languagesSelect.selectByVisibleText("Python");
+
+        // let's get all selected options
+
+        List<WebElement> selectedLanguages=languagesSelect.getAllSelectedOptions();
+
+        for(WebElement eachSelectedLanguage:selectedLanguages){
+            System.out.println(eachSelectedLanguage.getText());
+        }
+
+
+
+
+
+        BrowserUtils.wait(3);
+        driver.quit();
+
+    }
+}
