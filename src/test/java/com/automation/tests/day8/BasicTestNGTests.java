f542e5ae443498f9f6c248d3440e4e8889bb8aa2
mugeorman <mugeorman@gmail.com>
2020-03-14 13:20:31 -0400

    TestNG- before method-after method annotations

--- src/test/java/com/automation/tests/day8/BasicTestNGTests.java
+++ src/test/java/com/automation/tests/day8/BasicTestNGTests.java
@@ -1,9 +1,47 @@
 package com.automation.tests.day8;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class BasicTestNGTests {
 
 
+    //RUNS BEFORE EVERY TEST AUTOMATICALLY
+    // works as a pre-condition or setup
+    @BeforeMethod
+    public void setup(){
+    System.out.println("BEFORE METHOD");
+    }
 
+    // RUNS AUTOMATICALLY AFTER EVERY TEST
+    @AfterMethod
+    public void teardown(){
+        System.out.println("AFTER METHOD");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("TEST 1");
+        String expected="apple";
+        String actual="apple";
+        Assert.assertEquals(expected,actual);
+
+
+
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("TEST 2");
+        int num1=5;
+        int num2=10;
+        // it calls hard assertion
+        // if assertion fails- it stops the execution(due to exception)
+        Assert.assertTrue(num1<num2);
+
+    }
 
 
 
