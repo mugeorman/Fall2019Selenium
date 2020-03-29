d25428a6f88ac41065fdd6d035aa53b5d6e622e8
mugeorman <mugeorman@gmail.com>
2020-03-29 12:46:05 -0400

    day 15- CalendarsEventPage

--- src/test/java/com/automation/pages/AbstractPageBase.java
+++ src/test/java/com/automation/pages/AbstractPageBase.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
@@ -22,10 +24,21 @@ public  abstract class AbstractPageBase {
     protected WebDriver driver=Driver.getDriver();
     protected WebDriverWait wait =new WebDriverWait(driver,15);
 
+    @FindBy(css="#user-menu >a")
+    protected WebElement currentUser;
+
        public AbstractPageBase(){
 
            PageFactory.initElements(driver,this);
 
+       }
+
+
+       public String getCurrentUserName(){
+
+           BrowserUtils.waitForPageToLoad(10);
+           wait.until(ExpectedConditions.visibilityOf(currentUser));
+           return currentUser.getText().trim();
        }
 
 
@@ -55,6 +68,8 @@ public  abstract class AbstractPageBase {
         // increase this wait time if it is failing
         BrowserUtils.wait(3);
     }
+
+
 
 
 }
--- src/test/java/com/automation/pages/activities/CalendarsEventPage.java
+++ src/test/java/com/automation/pages/activities/CalendarsEventPage.java
@@ -1,6 +1,7 @@
 package com.automation.pages.activities;
 
 import com.automation.pages.AbstractPageBase;
+import com.automation.utilities.BrowserUtils;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,8 +14,22 @@ public class CalendarsEventPage extends AbstractPageBase {
     private WebElement createCalendarEvent;
 
 
+    @FindBy(className="select2-chosen")
+    private WebElement owner;
+
+    public String getOwnerName(){
+
+        BrowserUtils.waitForPageToLoad(10);
+        wait.until(ExpectedConditions.visibilityOf(owner));
+
+        return owner.getText().trim();
+    }
+
+
+
     public void clickToCreateCalendarEvent(){
 
+        BrowserUtils.waitForPageToLoad(10);
         wait.until(ExpectedConditions.elementToBeClickable(createCalendarEvent)).click();
 
 
